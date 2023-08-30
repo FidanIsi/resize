73cd1ad5fde299388afa8b45ef2f7b35e9ae391b
fidan <fidanji@code.edu.az>
2023-08-30 15:59:08 +0400

    addremove

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp1
+{
+
+    public class ArrayHelper
+    {
+        public static void Add(ref string[] arr, string input)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = input;
+        }
+
+        public static void Remove(ref string[] arr, string input)
+        {
+            int removingIndex = Array.IndexOf(arr, input);
+            if (removingIndex >= 0)
+            {
+                for (int i = removingIndex; i < arr.Length - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+
+                Array.Resize(ref arr, arr.Length - 1);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayHelper = new string[0];
+            string input1 = "This";
+            string input2 = "is";
+            string input3 = "new";
+            string input4 = "input";
+
+            ArrayHelper.Add(ref arrayHelper, input1);
+            ArrayHelper.Add(ref arrayHelper, input2);
+            ArrayHelper.Add(ref arrayHelper, input3);
+            ArrayHelper.Add(ref arrayHelper, input4);
+
+            foreach (string input in arrayHelper)
+            {
+                Console.WriteLine(input);
+            }
+
+
+            ArrayHelper.Remove(ref arrayHelper, input1);
+
+            foreach (string input in arrayHelper)
+            {
+                Console.WriteLine(input);
+            }
+
+        }
+    }
+}
